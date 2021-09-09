3a96efce67784a3e29878f4c5e362217f5db84ca
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2021-09-09 16:58:00 +0100

    #4: test fetching info from the local database

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,33 @@
-import { Client, Pool } from 'pg';
+import { Pool } from 'pg';
 import express from 'express';
 
 const PROD = (process.env.PORT) ? true : false;
 const PORT = process.env.PORT || 8000;
 
 const app = express();
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,
+	ssl: PROD ? { rejectUnauthorized: false } : false,
+});
 
 app.get('/', (req, res) => {
 	res.send('Hello World');
 });
 
+app.get('/apitest', async (req, res) => {
+	try {
+		const client = await pool.connect();
+		const result = await client.query('SELECT * FROM user_projects');
+		const results = { results: (result) ? result.rows : null };
+		res.send(results);
+		client.release();
+	} catch (err) {
+		console.error(err);
+		res.send(err);
+	}
+});
+
 app.listen(PORT, () => {
-	console.log(`Server started at http://localhost:${port}`);
+	console.log(`Server started at http://localhost:${PORT}`);
 });
 
-const client = new Client({
-	connectionString: process.env.DATABASE_URL,
-	ssl: PROD ? { rejectUnauthorized: false } : false,
-});
-
-client.connect()
-	.then(() => {
-		client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-			if (err) throw err;
-			for (let row of res.rows) {
-				console.log(JSON.stringify(row));
-			}
-			client.end();
-		});
-	})
-	.catch((error) => {
-		console.log(error);
-	});

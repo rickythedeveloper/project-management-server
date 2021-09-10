b182fa585b01e561dd725c600058ee76d8be75cb
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2021-09-10 16:33:20 +0100

    #16: Disallow adding a project of the same name for the same user

--- src/database/database.ts
+++ src/database/database.ts
@@ -57,6 +57,10 @@ export const addUser = async (user: OmitID<UserAccount>): Promise<UserAccount> =
 export const addProjectToUser = async (project: OmitID<Project>): Promise<{ project: Project; userProject: UserProject }> => {
 	if (!await rowWithIDExists(Table.user_accounts, project.owner_user_id)) throw new Error('Cannot add a project to a non-existent user.');
 
+	const projectNameResults = await pool.query<Pick<Project, 'name'>>('SELECT name FROM projects WHERE owner_user_id=$1', [project.owner_user_id]);
+	const projectNames = projectNameResults.rows.map(row => row.name);
+	if (projectNames.indexOf(project.name) !== -1) throw new Error('Cannot add a project of the same name for the same user');
+
 	return makeMultiQuery(async (client) => {
 		const addProjectQuery: QueryConfig = {
 			text: `INSERT INTO ${Table[Table.projects]} (name, owner_user_id)  VALUES ($1, $2) RETURNING *`,

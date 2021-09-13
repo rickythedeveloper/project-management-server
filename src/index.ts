75f693778ff24ee610a0eaac270a46b4b0bcfd43
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2021-09-13 14:01:35 +0100

    #25 #26: get / edit / delete metric and metric_option. Close #25. Close #26.

--- src/index.ts
+++ src/index.ts
@@ -32,12 +32,20 @@ app.delete('/projects/:id', projectsAPI.del);
 app.post('/tickets', ticketsAPI.post);
 app.get('/tickets', (req, res) => { dataTableAPI.getAll(DataTable.tickets, res); });
 app.get('/tickets/:id', (req, res) => { dataTableAPI.getOne(DataTable.tickets, req, res); });
-app.put('/tickets/:id', (req, res) => { dataTableAPI.edit(DataTable.tickets, ['project_id', 'created_user_id', 'index_in_project'], req, res); });
+app.put('/tickets/:id', (req, res) => { dataTableAPI.edit(DataTable.tickets, ['id', 'project_id', 'created_user_id', 'index_in_project'], req, res); });
 app.delete('/tickets/:id', (req, res) => { dataTableAPI.del(DataTable.tickets, req, res); });
 
 app.post('/metrics', metricsAPI.post);
+app.get('/metrics', (req, res) => { dataTableAPI.getAll(DataTable.metrics, res); });
+app.get('/metrics/:id', (req, res) => { dataTableAPI.getOne(DataTable.metrics, req, res); });
+app.put('/metrics/:id', (req, res) => {dataTableAPI.edit(DataTable.metrics, ['id', 'project_id'], req, res); });
+app.delete('/metrics/:id', (req, res) => { dataTableAPI.del(DataTable.metrics, req, res); });
 
 app.post('/metric-options', metricOptionsAPI.post);
+app.get('/metric-options', (req, res) => { dataTableAPI.getAll(DataTable.metric_options, res); });
+app.get('/metric-options/:id', (req, res) => { dataTableAPI.getOne(DataTable.metric_options, req, res); });
+app.put('/metrics-options/:id', (req, res) => { dataTableAPI.edit(DataTable.metric_options, ['id', 'metric_id'], req, res); });
+app.delete('/metric-options/:id', (req, res) => { dataTableAPI.del(DataTable.metric_options, req, res); });
 
 app.post('/ticket-assignees', ticketAssigneesAPI.post);
 
